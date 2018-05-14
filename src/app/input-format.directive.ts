ba660f480191deab755ba7c99d22f211a382b3f5
prescott breeden <pb@prescotts-iMac.local>
2018-05-13 18:58:58 -0700

    custom directives

---
+++ src/app/input-format.directive.ts
@@ -0,0 +1,25 @@
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appInputFormat]'
+})
+export class InputFormatDirective {
+  // tslint:disable-next-line:no-input-rename
+  // @Input('appInputFormat') format;
+  @Input('format') format;
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('blur') onBlur() {
+    const value: string = this.el.nativeElement.value;
+
+    if (this.format === 'lowercase') {
+      this.el.nativeElement.value = value.toLowerCase();
+    } else {
+      this.el.nativeElement.value = value.toUpperCase();
+    }
+  }
+        // @HostListener('focus') onFocus() {
+        //   console.log('on Focus');
+        // }
+}
